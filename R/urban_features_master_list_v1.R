e4c314ca8a7c14d90f1aba52e43c4fbd7e8fcd37
Tiziana A. Gelmi Candusso <44434943+tgelmi-candusso@users.noreply.github.com>
2023-02-06 11:33:37 -0500

    sidewalk out
    sidewalk out

--- R/urban_features_master_list_v1.R
+++ R/urban_features_master_list_v1.R
@@ -99,14 +99,14 @@ df_unique_res <- df_unique %>%
   mutate(priority = ifelse(feature == "linear_feature_rail" & type %in% c('tram'), 18, priority))%>%
   ##pedestrian roads #allows for overpasses and underpasses by being set with higher priority as roads
   mutate(priority = ifelse(feature == "linear_feature_no_traffic" & material != 'sidewalk', 19, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_no_traffic" & material == 'sidewalk', 20, priority))%>%
+  #mutate(priority = ifelse(feature == "linear_feature_no_traffic" & material == 'sidewalk', 20, priority))%>%
   ##railways
-  mutate(priority = ifelse(feature == "linear_feature_rail", 21, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_rail"& type %in% c('abandoned','disused','construction'), 22, priority))%>%
+  mutate(priority = ifelse(feature == "linear_feature_rail", 20, priority))%>%
+  mutate(priority = ifelse(feature == "linear_feature_rail"& type %in% c('abandoned','disused','construction'), 21, priority))%>%
   ##barriers (too thin to appear on 30m resolution layer, useful for other purposes) 
-  mutate(priority = ifelse(feature == "barrier", 23, priority))%>%
+  mutate(priority = ifelse(feature == "barrier", 22, priority))%>%
   ##flooded surface (note includes wetlands, if wetlands want to be separated sql code should be changed)
-  mutate(priority = ifelse(feature == "water", 24, priority)) 
+  mutate(priority = ifelse(feature == "water", 23, priority)) 
 
 
 
@@ -144,7 +144,7 @@ df_unique_res <- df_unique_res %>%
   mutate(resistance = ifelse(feature == "linear_feature_rail"& type %in% c('tram'), 45, resistance))%>%
   ##pedestrian roads #allows for overpasses and underpasses by being set with higher resistance as roads
   mutate(resistance = ifelse(feature == "linear_feature_no_traffic"& material != 'sidewalk', 15, resistance))%>%
-  mutate(resistance = ifelse(feature == "linear_feature_no_traffic"& material == 'sidewalk', 20, resistance))%>%
+  #mutate(resistance = ifelse(feature == "linear_feature_no_traffic"& material == 'sidewalk', 20, resistance))%>%
   ##railways
   mutate(resistance = ifelse(feature == "linear_feature_rail", 15, resistance))%>%
   mutate(resistance = ifelse(feature == "linear_feature_rail"& type %in% c('abandoned','disused','construction'), 10, resistance))%>%
