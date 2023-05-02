373d382e1a17ae8f91fc4e2d19a07a3096e94adc
Tiziana A. Gelmi Candusso <44434943+tgelmi-candusso@users.noreply.github.com>
2023-05-02 16:22:18 -0400

    fixed priorities with new layers

--- R/urban_features_master_list_v1.R
+++ R/urban_features_master_list_v1.R
@@ -76,57 +76,48 @@ head(df_unique)
 
 
 ##set priorities to avoid conflicting resistance values within the same priority level
+##set priorities to avoid conflicting resistance values within the same priority level
 df_unique_res <- df_unique %>% 
-  #priority values define how features are overlaid in the final map, with higher values being on top
-  ##landuse background
-  mutate(priority = ifelse(feature == "commercial_industrial" & (type %in% c("industrial", "fairground") | size == "factory"), 1, 'NULL')) %>% #Why NULL and not NA?
-  mutate(priority = ifelse(feature == "commercial_industrial" & type %in% c("commercial", "retail"), 2, priority)) %>%
-  mutate(priority = ifelse(feature == "institutional", 3, priority))%>%
-  mutate(priority = ifelse(feature == "residential", 4, priority))%>%
-  mutate(priority = ifelse(feature == "landuse_rail", 5, priority))%>%
-  ##green background, now includes protected areas as a layer to account those protected areas where vegetation is not described (this could be superceded by the CEC landcover
-  mutate(priority = ifelse(feature == "open_green_area", 6, priority))%>%
-  mutate(priority = ifelse(feature == "protected_area", 7, priority))%>%  
-  mutate(priority = ifelse(feature == "resourceful_green_area", 8, priority))%>%  
-  mutate(priority = ifelse(feature == "hetero_green_area" & !(material %in% c('sand','scree','sinkhole', 'beach') | 
-                                                             type %in% c('brownfield', 'construction') | 
-                                                             size %in% c('bunker')), 9, priority)) %>%
-  ##bare non-vegetated-non-concrete layer
-  mutate(priority = ifelse(feature == "hetero_green_area" & (material %in% c('sand','scree','sinkhole', 'beach') | 
-                                                             type %in% c('brownfield', 'construction') | 
-                                                             size %in% c('bunker')), 10 , priority))%>%
-
-  mutate(priority = ifelse(feature == "dense_green_area", 11, priority))%>%
-  ##water, does not include temporary retention basins
-  mutate(priority = ifelse(feature == "water", 12, priority))%>% 
-  ##built infrastructure
-  mutate(priority = ifelse(feature == "parking_surface", 13, priority))%>%
-  mutate(priority = ifelse(feature == "building", 14, priority))%>%
-  ##roads - highways go below other linear features to allow for over and underpasses
-  mutate(priority = ifelse(feature == "linear_feature_vh_traffic",15, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_no_traffic" & material == 'sidewalk', 16, priority))%>% ### sidewalks.
-  mutate(priority = ifelse(feature == "linear_feature_na_traffic", 17, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_vl_traffic",18, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_l_traffic", 19, priority))%>%  
-  mutate(priority = ifelse(feature == "linear_feature_m_traffic", 20, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_h_traffic_ls", 21, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_h_traffic_hs", 22, priority))%>%
-  #trams included here
-  mutate(priority = ifelse(feature == "linear_feature_rail" & type == 'tram', 23, priority))%>% ## this layer did not appear, should debug code
-  ##pedestrian roads #allows for overpasses and underpasses by being set with higher priority as roads
-  mutate(priority = ifelse(feature == "linear_feature_no_traffic" & material != 'sidewalk', 24, priority))%>% ### sidewalks.
-
-  #mutate(priority = ifelse(feature == "linear_feature_no_traffic" & material == 'sidewalk', 20, priority))%>%
-  ##railways
-  mutate(priority = ifelse(feature == "linear_feature_rail", 25, priority))%>%
-  mutate(priority = ifelse(feature == "linear_feature_rail" & (type %in% c('abandoned','disused','construction') | material == 'construction'), 26, priority))%>%
-  ##barriers (too thin to appear on 30m resolution layer, useful for other purposes) 
-  mutate(priority = ifelse(feature == "barrier", 27, priority)) ## the
+  mutate(priority = case_when(
+    df_unique$feature == "commercial_industrial" & !(df_unique$type %in% c("commercial", "retail")) ~ '1',
+    df_unique$feature == "commercial_industrial" & df_unique$type %in% c("commercial", "retail") ~ '2',
+    df_unique$feature == "institutional"  ~  '3',
+    df_unique$feature == "residential"  ~  '4',
+    df_unique$feature == "landuse_rail"  ~  '5',
+    df_unique$feature == "open_green_area"  ~  '6',
+    df_unique$feature == "protected_area"  ~  '7', 
+    df_unique$feature == "resourceful_green_area"  ~  '8',
+    df_unique$feature == "hetero_green_area"  ~  '9', #non_bare_soil
+    df_unique$feature == "bare_soil"  ~   '10' , #bare_soil
+    df_unique$feature == "dense_green_area"  ~   '11',
+    df_unique$feature == "water"  ~   '12', 
+    df_unique$feature == "waterways"  ~   '12', ### hope this wont create trouble, they are both supposed to be at the same level
+    df_unique$feature == "parking_surface"  ~ '13',
+    df_unique$feature == "building"  ~  '14',
+    df_unique$feature == "linear_feature_vh_traffic"  ~  '15',
+    df_unique$feature == "linear_feature_no_traffic" & df_unique$material == 'sidewalk'  ~  '16', # sidewalks
+    df_unique$feature == "linear_feature_na_traffic"  ~   '17',
+    df_unique$feature == "linear_feature_vl_traffic"  ~  '18',
+    df_unique$feature == "linear_feature_l_traffic"  ~  '19',
+    df_unique$feature == "linear_feature_m_traffic"  ~  '20',
+    df_unique$feature == "linear_feature_h_traffic_ls"  ~  '21',
+    df_unique$feature == "linear_feature_h_traffic_hs"  ~  '22',
+    df_unique$feature == "linear_feature_rail" & df_unique$type == 'tram'  ~  '23', # tram_lines
+    df_unique$feature == "linear_feature_no_traffic" & !(df_unique$material %in% c('sidewalk'))  ~  '24', # except sidewalks
+    df_unique$feature == "linear_feature_rail" & !(df_unique$type %in% c('tram','abandoned','disused','construction'))
+                                                   & !(df_unique$material %in% c('construction')) ~  '25',
+    df_unique$feature == "linear_feature_rail" & 
+      (df_unique$type %in% c('abandoned','disused','construction') | df_unique$material == 'construction')  ~  '26',
+    df_unique$feature == "barrier"  ~  '27'))
 
 
+#View(df_unique_res)
+df_unique_res$priority<-as.numeric(df_unique_res$priority)
+priority_table <- df_unique_res %>% dplyr::count(priority, feature, .drop=FALSE)
+df_unique_res%>% filter(feature == "linear_feature_no_traffic" & is.na(priority))
 
-# Add new column "class" 
-df_unique_res$class <- df_unique_res$priority
+write.csv(df_unique_res, 'df_unique_res.csv')
+
 
 # Save object
 saveRDS(df_unique_res, "../misc/df_unique_res.rds")
