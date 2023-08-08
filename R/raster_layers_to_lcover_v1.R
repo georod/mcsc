de8ee2b648b71166727bf7562ca49d2ad65a6b20
georod <p.rodriguez97@gmail.com>
2023-08-08 17:01:08 -0400

    Layer to raster

--- R/raster_layers_to_lcover_v1.R
+++ R/raster_layers_to_lcover_v1.R
@@ -89,7 +89,7 @@ city$pg_city <- gsub(" ", "_", city$osm_city)
 #city <- city[(city$pg_city %in% c('Toronto')), 6]
 #city <- city[1:4 ,6]
 #city <- city[6:10 ,6]
-city <- city[11:25 ,6] # cec2 added
+city <- city[c(11:30,32:35) ,6] # cec2 added
 
 #=================================
 # Connect to PG db - STEP 2
