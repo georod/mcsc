e79e620ef87926deef28a8daa4ce4e0a281cd362
georod <p.rodriguez97@gmail.com>
2024-03-21 15:03:16 -0400

    Add city env sq

--- R/city_envelopes_squirrels_v1.R
+++ R/city_envelopes_squirrels_v1.R
@@ -79,7 +79,7 @@ city$pg_city <- gsub(" ", "_", city$osm_city)
 #city <- city[(city$pg_city %in% c('Victoria')),]
 #city <- city[(city$pg_city %in% c('Jackson')),]
 #city <- city[(city$pg_city %in% c('Key_Largo')),]
-city <- city[(city$pg_city %in% c('Golden_Horseshoe')),]
+#city <- city[(city$pg_city %in% c('Golden_Horseshoe')),]
 #print(city)
 
 # Loop for creating city spatial envelopes
